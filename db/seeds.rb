08f86c58818aac8b74c81dc26dab123ecb7b6c77
DESKTOP-MDM8KAA\lx <bianyuqi142857@gmail.com>
2022-04-07 23:16:18 +0800

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create!(name: "Example User",
-	email: "example@railstutorial.org",
+User.create!(name: "Jacky",
+	email: "qiyu@qq.org",
 	password: "foobar",
 	password_confirmation: "foobar",
 	admin: true, activated: true,
@@ -21,3 +21,11 @@ users = User.order(:created_at).take(6)
 	content = Faker::Lorem.sentence(word_count: 5)
 	users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
       password: "foobar", password_confirmation:"foobar")
   end
   test "should be valid" do
-  assert @user.valid?
+    assert @user.valid?
   end
 
   test "name should not be present" do 
@@ -28,34 +28,34 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   test "email validation should reject invalid addresses" do
-  invalid_addresses = %w[user@example,com user_at_foo.org
-  user.name@example.
-  foo@bar_baz.com foo@bar+baz.com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be
-      invalid"
+    invalid_addresses = %w[user@example,com user_at_foo.org
+      user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        assert_not @user.valid?, "#{invalid_address.inspect} should be
+        invalid"
+      end
     end
-  end
 
-  test "email addresses should be unique" do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
-    assert_not duplicate_user.valid?
-  end
+    test "email addresses should be unique" do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      assert_not duplicate_user.valid?
+    end
 
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
-  end
+    test "password should be present (nonblank)" do
+      @user.password = @user.password_confirmation = " " * 6
+      assert_not @user.valid?
+    end
+    test "password should have a minimum length" do
+      @user.password = @user.password_confirmation = "a" * 5
+      assert_not @user.valid?
+    end
 
-  test "authenticated? should return false for a user with nil
-  digest" do
+    test "authenticated? should return false for a user with nil
+    digest" do
     assert_not @user.authenticated?(:remember, '')
   end
 
@@ -67,4 +67,35 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+# Posts from followed user
+lana.microposts.each do |post_following|
+  assert michael.feed.include?(post_following)
+end
+# Posts from self
+michael.microposts.each do |post_self|
+  assert michael.feed.include?(post_self)
+end
+# Posts from unfollowed user
+archer.microposts.each do |post_unfollowed|
+  assert_not michael.feed.include?(post_unfollowed)
+end
+end
 end
